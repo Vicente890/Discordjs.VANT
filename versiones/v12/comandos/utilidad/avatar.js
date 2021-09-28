d6ea5ea0bca3dd83889d1880fee037c302937dc0
Vicens <74480467+Vicente890@users.noreply.github.com>
2021-09-28 18:51:56 -0500

    Update avatar.js

--- versiones/v12/comandos/utilidad/avatar.js
+++ versiones/v12/comandos/utilidad/avatar.js
@@ -8,7 +8,7 @@ module.exports = {
   alias: ["av","ava"], //si no tienes ningun alias dejalo vacio, osea, sin "", solo []
 
 execute (client, message, args){
-  const user = message.mentions.users.first() || message.author || client.users.cache.fetch(args[0])
+  const user = message.mentions.users.first() || message.author;
   const avatar = user.displayAvatarURL({dynamic:true})
 const embed = new MessageEmbed()
 .setImage(avatar)
